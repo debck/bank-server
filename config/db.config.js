e82871c39869dbe0352698331e91459e624f7d0c
debck <debck7019@gmail.com>
2021-01-07 17:34:38 +0530

    edit

--- config/db.config.js
+++ config/db.config.js
@@ -2,9 +2,9 @@ const Pool = require('pg').Pool
 
 
 exports.pool = new Pool({ 
-  user: 'u4trlhyarzxuwstipdmx',
-  host: 'b6kfwvd51hlpskrseqjw-postgresql.services.clever-cloud.com',
-  database: 'b6kfwvd51hlpskrseqjw',
-  password: 'PbnhqETA6rXOapdS1pz5',
+  user: '',
+  host: '',
+  database: '',
+  password: '',
   port: 5432
 })
